3386a47059d757ddf3c67f4bea45b0d4541789bc
Willy Jacquet <willy.jacquet@outlook.com>
2022-02-08 12:48:28 +0100

    (Opt)VertexArrayName

---
+++ src/agl/api/opengl/vertex_array/type/optional_vertex_array_name.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace agl::opengl {
+
+/**
+ * Invariants:
+ * - (TODO) OpenGL resource ??
+ */
+class OptVertexArrayName {
+    GLuint name = GL_NONE;
+
+public:
+
+    OptVertexArrayName() = default;
+
+    // Should check that 'name' is an OpenGL name ??
+    OptVertexArrayName(GLuint name)
+        : name(name)
+    {}
+
+    operator GLuint() const noexcept {
+        return name;
+    }
+};
+
+}
---
+++ src/agl/api/opengl/vertex_array/type/vertex_array_name.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace agl::opengl {
+
+/**
+ * Invariants:
+ * - (TODO) OpenGL resource.
+ */
+class VertexArrayName {
+    GLuint name;
+
+public:
+
+    VertexArrayName() = delete;
+
+    // Shoudl check that 'name' is an OpenGL name.
+    VertexArrayName(GLuint name)
+        : name(name)
+    {}
+
+    operator GLuint() const noexcept {
+        return name;
+    }
+};
+
+}

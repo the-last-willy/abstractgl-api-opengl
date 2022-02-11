8fae203154103dfdd54fce241c9db85a4ccd595a
Willy Jacquet <willy.jacquet@outlook.com>
2022-02-11 17:44:22 +0100

    Fixed UseProgram

--- src/abstractgl/api/opengl/program/function/core/use_program.hpp
+++ src/abstractgl/api/opengl/program/function/core/use_program.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "../../type/optional_program_name.hpp"
+#include "../../type/core/program_name.hpp"
 
 namespace agl::api::opengl {
 
 inline
 void
-LinkProgram(
-    OptProgramName program)
+UseProgram(
+    ProgramName program)
 {
-    glLinkProgram(program);
+    glUseProgram(program);
 }
 
 }
